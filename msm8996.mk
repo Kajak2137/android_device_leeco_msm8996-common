2cd74cd1e8763d16c86ff90c83ecffd4701af5d0
Kajak <kajos200@gmail.com>
2017-09-08 22:05:44 +0200

    AOSP-ify

--- msm8996.mk
+++ msm8996.mk
@@ -105,14 +105,6 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     tinymix
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Camera
 PRODUCT_PACKAGES += \
